db15244d23cbda9aa45843a73880d29bbbcc7fdd
Tomas Mejia <tomas.mejia.ed@gmail.com>
2019-03-11 21:42:26 -0300

    fix regex and custom image url validator bugs

--- src/app/upload/imageUrl.validators.ts
+++ src/app/upload/imageUrl.validators.ts
@@ -1,10 +1,10 @@
 import { AbstractControl, ValidationErrors } from '@angular/forms';
 
-const regexForImages = '/\.(jpeg|jpg|png|gif)/g)';
+const regexForImages = /\.(jpeg|jpg|png|gif)/g;
 
 export class ImageUrlValidators {
   static mustBeValidUrl(control: AbstractControl): ValidationErrors|null {
-    if ((control.value as string).match(regexForImages)) {
+    if (!(control.value as string).match(regexForImages)) {
         return { mustBeValidUrl: true };
     }
     return null;
