004ea67fa282a4be5835b28117e125e4d9971f8d
Tomas Esteban Mejia Diaz <tomas.mejia@globant.com>
2019-03-12 15:06:19 -0300

    add setTimeOut to wait for redirection

--- src/app/image.service.ts
+++ src/app/image.service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@angular/core';
-
+import { Router } from '@angular/router';
 
 const IMAGES = [
-  {id: '1', caption: 'Hello world', url: 'https://i.imgur.com/YsqdBYc.jpg'},
-  {id: '2', caption: 'Nice meme', url: 'https://i.imgur.com/WWNb9Fh.jpg'},
-  {id: '3', caption: 'I don\'t know about this', url: 'https://i.imgur.com/E7PPNfB.jpg'},
-  {id: '4', caption: 'How low', url: 'https://i.imgur.com/xxO3aus.png'},
+  { id: '1', caption: 'Hello world', url: 'https://i.imgur.com/YsqdBYc.jpg' },
+  { id: '2', caption: 'Nice meme', url: 'https://i.imgur.com/WWNb9Fh.jpg' },
+  {
+    id: '3',
+    caption: "I don't know about this",
+    url: 'https://i.imgur.com/E7PPNfB.jpg'
+  },
+  { id: '4', caption: 'How low', url: 'https://i.imgur.com/xxO3aus.png' }
 ];
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageService {
-
   getImages() {
     return IMAGES;
   }
@@ -25,10 +28,13 @@ export class ImageService {
     const image = {
       id: '5',
       url: form.imageUrl,
-      caption: form.caption,
+      caption: form.caption
     };
-    return IMAGES.unshift(image);
+    IMAGES.unshift(image);
+    setTimeout(() => {
+      return this.router.navigate(['/']);
+    }, 2000);
   }
 
-  constructor() { }
+  constructor(private router: Router) {}
 }
