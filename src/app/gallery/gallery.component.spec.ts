6acd26be395f5b504c6cdaa7935b957c833314be
Tomas Esteban Mejia Diaz <tomas.mejia@globant.com>
2019-03-15 15:09:35 -0300

    remove fit and fdescribe frmo gallery component

--- src/app/gallery/gallery.component.spec.ts
+++ src/app/gallery/gallery.component.spec.ts
@@ -4,7 +4,7 @@ import { GalleryComponent } from './gallery.component';
 import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 
-fdescribe('GalleryComponent', () => {
+describe('GalleryComponent', () => {
   let component: GalleryComponent;
   let fixture: ComponentFixture<GalleryComponent>;
 
@@ -24,14 +24,14 @@ fdescribe('GalleryComponent', () => {
     fixture.detectChanges();
   });
 
-  fit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  fit('should get the pictures array', () => {
+  it('should get the pictures array', () => {
     expect(component.visibleImages).not.toBeUndefined();
   });
-  fit('should render the pictures', () => {
+  it('should render the pictures', () => {
     const elements = fixture.debugElement.queryAll(By.css('.gallery__item'));
     expect(elements).not.toBeNull();
   });
