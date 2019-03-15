de19b85764f361337a316b3dfbac1adacad7215a
Tomas Esteban Mejia Diaz <tomas.mejia@globant.com>
2019-03-15 15:08:10 -0300

    add testing for gallery component

--- src/app/gallery/gallery.component.spec.ts
+++ src/app/gallery/gallery.component.spec.ts
@@ -1,13 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GalleryComponent } from './gallery.component';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
 
-describe('GalleryComponent', () => {
+fdescribe('GalleryComponent', () => {
   let component: GalleryComponent;
   let fixture: ComponentFixture<GalleryComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+      ],
       declarations: [ GalleryComponent ]
     })
     .compileComponents();
@@ -19,7 +24,15 @@ describe('GalleryComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  fit('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  fit('should get the pictures array', () => {
+    expect(component.visibleImages).not.toBeUndefined();
+  });
+  fit('should render the pictures', () => {
+    const elements = fixture.debugElement.queryAll(By.css('.gallery__item'));
+    expect(elements).not.toBeNull();
   });
 });
